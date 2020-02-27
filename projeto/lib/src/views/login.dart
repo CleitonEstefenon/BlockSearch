2412dd0801577f8a7140a9a1a7d75422495ae537
Cleiton Estefenon <cleitonestefenon@gmail.com>
2020-02-26 23:51:06 -0300

    Envolvido os Widgets do login em um Form para implementar as validações

--- projeto/lib/src/views/login.dart
+++ projeto/lib/src/views/login.dart
@@ -13,55 +13,61 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          Image.asset(
-            "images/logo.png",
-            width: double.maxFinite,
-            height: 200,
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextFormField(
-              controller: loginBloc.emailController,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                border: OutlineInputBorder()
+      body: Form(
+        key: null,
+        child: SingleChildScrollView(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, 
+            children: <Widget>[
+              Image.asset(
+                "images/logo.png",
+                width: double.maxFinite,
+                height: 200,
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextFormField(
-              obscureText: true,
-              controller: loginBloc.passwordController,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                border: OutlineInputBorder()
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: TextFormField(
+                  controller: loginBloc.emailController,
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    border: OutlineInputBorder()
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
               ),
-              keyboardType: TextInputType.text,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: RaisedButton(
-              padding: EdgeInsets.all(20),
-              color: Theme.of(context).primaryColor,
-              child: Text(
-                "Entrar",
-                style: TextStyle(color: Colors.white),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: loginBloc.passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    border: OutlineInputBorder()
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
               ),
-              onPressed: () {
-                setState(() {
-                  if(loginBloc.fazerLogin()) {
-                    print('LOGIN EFETUADO COM SUCESSO!');
-                  }
-                });
-              },
-            ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: RaisedButton(
+                  padding: EdgeInsets.all(20),
+                  color: Theme.of(context).primaryColor,
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if(loginBloc.fazerLogin()) {
+                        print('LOGIN EFETUADO COM SUCESSO!');
+                      }
+                    });
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
