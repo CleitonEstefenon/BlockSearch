42a58e6bc7a0eccfd4b9a04b05d24a91910bd70e
Cleiton Estefenon <cleitonestefenon@gmail.com>
2020-05-03 11:12:19 -0300

    Melhorias na função de registrar documentos

--- projeto/lib/src/services/checkConnection.dart
+++ projeto/lib/src/services/checkConnection.dart
@@ -1,53 +1,39 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:projeto/src/helpers/SharedPrefKey.dart';
+import 'package:projeto/src/helpers/SharedPrefUser.dart';
 
-class User {
-  String message;
-  String token;
-  String name;
-  String email;
-  String statusCode;
+class Document {
+  Future register(String pathFile) async {
+    String token = await getStringUserPreferences(SharedPreferencesKey.TOKEN);
 
-  User({this.message, this.token, this.name, this.email, this.statusCode});
+    var url = 'https://blockshare-backend.herokuapp.com/transaction';
 
-  User.fromJson(Map<String, dynamic> json) {
-    message = json['message'];
-    token = json['token'];
-    name = json['name'];
-    email = json['email'];
-    statusCode = json['statusCode'];
-  }
-}
+    Map<String, String> header = {
+      "Content-Type": "multipart/form-data",
+      "Authorization": "Bearer " + token
+    };
 
-class LoginApi {
-  static Future<User> validationLoginApi(String email, String password) async {
-    // Conexão local Host. Trocar o IP pelo IP da maquina que está executando e liberar a porta 3333 no firewall
-    //var url = 'http://192.168.0.103:3333/authentication';
+    Dio dio = new Dio();
+    var formdata =
+        FormData.fromMap({"file": await MultipartFile.fromFile(pathFile)});
 
-    //conexão no servidor heroku
-    var url = 'https://blockshare-backend.herokuapp.com/authentication';
-
-    var header = {"Content-Type": "application/json"};
-
-    Map params = {"email": email, "password": password};
-
-    var bodyJson = json.encode(params);
-
-    var response = await http.post(url, headers: header, body: bodyJson);
-
-    var responseMap = jsonDecode(response.body);
-
-    print('BUNDAAAA BUUNDAAA -> ${responseMap.messa}');
-
-    var user = new User.fromJson({
-      "message": responseMap.message,
-      "token": 'responseMap.token',
-      "name": 'responseMap.name',
-      "email": 'responseMap.email',
-      //"statusCode": response.statusCode,
+    dio
+        .post(
+      url,
+      data: formdata,
+      options: Options(
+        method: 'POST',
+        responseType: ResponseType.stream,
+        headers: header,
+      ),
+    )
+        .then((response) {
+      debugPrint(response.statusCode.toString());
+      debugPrint("SUCESSO");
+    }).catchError((error) {
+      debugPrint("DEU BOSTA");
+      debugPrint(error);
     });
-    print('AUISHDIAUSDIASIUD -> ${user.email}');
-   
-   return null;
   }
 }
--- projeto/lib/src/services/document.dart
+++ projeto/lib/src/services/document.dart
@@ -4,22 +4,27 @@ import 'package:projeto/src/helpers/SharedPrefKey.dart';
 import 'package:projeto/src/helpers/SharedPrefUser.dart';
 
 class Document {
+
   Future register(String pathFile) async {
+    
+    //pega o token do usuário salvo na SharedPeference
     String token = await getStringUserPreferences(SharedPreferencesKey.TOKEN);
 
     var url = 'https://blockshare-backend.herokuapp.com/transaction';
 
+    //monta o header e envia o token de autorização
     Map<String, String> header = {
       "Content-Type": "multipart/form-data",
       "Authorization": "Bearer " + token
     };
 
-    Dio dio = new Dio();
+    //carrega o documento do diretório local do celular.
     var formdata =
         FormData.fromMap({"file": await MultipartFile.fromFile(pathFile)});
-
-    dio
-        .post(
+    
+    //Faz o post para a API com todas as informações. 
+    Dio dio = new Dio();
+    dio.post(
       url,
       data: formdata,
       options: Options(
@@ -27,8 +32,7 @@ class Document {
         responseType: ResponseType.stream,
         headers: header,
       ),
-    )
-        .then((response) {
+    ).then((response) {
       debugPrint(response.statusCode.toString());
       debugPrint("SUCESSO");
     }).catchError((error) {
